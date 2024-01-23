3dd52b229651dafe9dcb106a2f82536afb3439ae
sadiya <idrishalyma04@gmail.com>
2024-01-23 22:54:12 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int temp = 39;
+        if (temp > 30) {
+            System.out.println("Its a hot day");
+            System.out.println("Drink plenty of water");
+        } else if (temp < 20 && temp >= 30) {
+            System.out.println("Its a nice day");
+        } else {
+            System.out.println("its cold");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
